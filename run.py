952dcca622f53a40d766c615ba9111c57021b742
Maruf Khan <whoismaruf@outlook.com>
2020-11-26 18:57:12 +0600

    Fix all issues

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from os import name
 from flask import Flask, jsonify, request, render_template
-from bot import predict_class, getResponse, intents
+from bot2 import predict_class, getResponse, intents
 
 app = Flask(__name__, template_folder='templates',static_folder='static')
 
